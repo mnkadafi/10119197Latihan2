f98c2f7e2aed8edf1be50c69e8166b893f20e984
Mochamad Nurkhayal Kadafi <wowkadafi@gmail.com>
2022-04-25 15:06:23 +0700

    Initial first time commit

---
+++ app/src/main/java/com/mnkadafi/a10119197latihan2/AlmostThereActivity.kt
@@ -0,0 +1,16 @@
+package com.mnkadafi.a10119197latihan2
+
+//<!--25 April 2022-->
+//<!--10119197-->
+//<!--Mochamad Nurkhayal Kadafi-->
+//<!--IF-5-->
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlmostThereActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_almost_there)
+    }
+}
